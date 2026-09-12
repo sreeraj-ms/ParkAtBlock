348f4cd81cb1d907bb21ff056d72b25e607d9fec
Rithik Raj <rithikraj@gmail.com>
2026-09-12 17:35:27 +0530

    Offine time 5 mints

--- ParkAtBlock.API/Configuration/ParkingSettings.cs
+++ ParkAtBlock.API/Configuration/ParkingSettings.cs
@@ -5,6 +5,6 @@ public sealed class ParkingSettings
     public const string SectionName = "ParkingSettings";
 
     public double OccupiedDistanceThresholdCm { get; set; } = 50;
-    public int DeviceOfflineTimeoutSeconds { get; set; } = 30;
+    public int DeviceOfflineTimeoutSeconds { get; set; } = 300;
     public string[] AllowedOrigins { get; set; } = [];
 }
